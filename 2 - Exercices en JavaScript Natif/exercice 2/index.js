780729f996bd8e2701fdb721b7c9a55a23ec58a1
Sofiane Fares <sofiane.fares77@outlook.fr>
2025-04-07 10:35:04 +0200

    J'ai tout mis dans un nouveau dossier + Exercice 2 : création d'un tableau avec des utilisateurs

---
+++ 2 - Exercices en JavaScript Natif/exercice 2/index.js
@@ -0,0 +1,25 @@
+const liste_user = [
+
+    {
+        "id" : 1,
+        "name": "Sofiane",
+        "username": "Sofianef",
+        "mail": "Sofianef@sofiane.f",
+        "age": 21,
+    },
+    {
+        "id" : 2,
+        "name": "Sofiane2",
+        "username": "Sofianef2",
+        "mail": "Sofianef2@sofiane.f",
+        "age": 21,
+    },
+    {
+        "id" : 4,
+        "name": "Sofiane3",
+        "username": "Sofianef3",
+        "mail": "Sofianef3@sofiane.f",
+        "age": 21,
+    }
+]
+
