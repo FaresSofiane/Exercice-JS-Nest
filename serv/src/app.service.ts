016c95f53dd397a387604b3696c96e9c00dd5995
Sofiane Fares <sofiane.fares77@outlook.fr>
2025-04-07 18:29:42 +0200

    .

--- serv/src/app.service.ts
+++ serv/src/app.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from './jwt-auth/jwt-auth.guard';
 
 @Injectable()
+@UseGuards(JwtAuthGuard)
 export class AppService {
   getHello(): string {
     return 'Hello World!';
